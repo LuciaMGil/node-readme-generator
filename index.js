71d238f38fe32ec01b58ca6160c5af48c1921060
Lucia Gil <luciagil5952@gmail.com>
2022-07-09 20:47:10 -0400

    Added validation

--- index.js
+++ index.js
@@ -7,41 +7,81 @@ inquirer.prompt([
         type: 'input',
         message: 'Enter your name:',
         name: 'name',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     {
         type: 'input',
         message: 'Enter the title of your application: ',
         name: 'title',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     {
         type: 'input',
         message: 'Enter a description of your application: ',
         name: 'description',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     {
         type: 'input',
         message: 'Create a table of contents: ',
         name: 'table',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     {
         type: 'input',
         message: 'Enter installation instructions for your application: ',
         name: 'installation',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     {
         type: 'input',
         message: 'Enter instructions on how to use the application: ',
         name: 'usage',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     {
         type: 'input',
         message: 'Enter contribution guidelines: ',
         name: 'contribution',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     {
         type: 'input',
         message: 'Enter test instructions: ',
         name: 'test',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     {
         type: 'list',
@@ -56,15 +96,27 @@ inquirer.prompt([
         type: 'input',
         message: 'Enter GitHub username: ',
         name: 'github',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     {
         type: 'input',
         message: 'Enter your email address: ',
         name: 'email',
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {return `Please enter a response`}
+        }
     },
     
 ])
 .then ((data) => {
     console.log(data)
     console.log(`program complete`)
+    console.log(answers)
 } )
+
